af01279b3d688156b22077802fa5511feedbe1c1
mhanby95 <114430857+mhanby95@users.noreply.github.com>
2022-10-14 16:59:41 -0500

    Add files via upload

---
+++ flaskgen.py
@@ -0,0 +1,44 @@
+
+from flask import Flask, render_template, request
+app = Flask(str(__name__))
+@app.route('/')
+def passgen():
+    return render_template('passgen.html')
+
+    import time
+    import string
+
+
+
+
+    for _ in range(10):
+        lis = string.ascii_letters + string.digits
+        pw = ''
+        n = 6
+
+
+        for i in range(n):
+            y = time.time()
+            savage = str(y).split('.')
+            savage = str(int(savage[1]) % 7)
+            
+
+            if int(savage) <= 5 and i < n-1 :
+                        idx = (int(savage)*10 + i) % len(lis)
+                        pw += lis[idx]
+            else:
+                        idx = ((int(savage)-5)*10 + i) % len(lis)
+                        pw += lis[idx]
+        print(pw)
+
+    return pw
+@app.route('/result',methods = ['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.formr
+        return render_template("result.html" ,result = result)
+
+if __name__ == "_main_":
+    app.run(debug = True)
+
+
