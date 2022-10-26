af0f948b2eb47dc0ef44ea32fab6c039bde137c5
mhanby95 <114430857+mhanby95@users.noreply.github.com>
2022-10-26 12:51:31 -0500

    Update Core.py

--- Core.py
+++ Core.py
@@ -1,6 +1,18 @@
-from math import pi 
-i = pi
-while i < 4:
-    print(i)
+def golf():
+    from math import pi
+    i = pi + (0.00000001)
+
+
+    while i > 3:
+        print(i)
+        
+        if True:
+            i += (0.00000001)
+            continue
+
+    
+
+    print(golf)
+golf()
     
 
